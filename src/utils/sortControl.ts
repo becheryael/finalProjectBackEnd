3d824c837b4b860d8e2b42faee99668d536bf3ed
YaelyB <becher.yael@gmail.com>
2026-01-31 12:30:02 +0200

    added error handling to all sortControl params

--- src/utils/sortControl.ts
+++ src/utils/sortControl.ts
@@ -7,25 +7,40 @@ const ALLOWED_TYPES = [
   "Sign for me"
 ];
 
+const isValidDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return !isNaN(date.getTime());
+};
+
 const sortControl = (query: any) => {
   let match: any = {};
   let sort: any = {};
 
-  if (query.status && ALLOWED_STATUSES.includes(query.status)) {
-    match.status = query.status;
+  if (query.status) {
+    if (ALLOWED_STATUSES.includes(query.status)) {
+      match.status = query.status;
+    } else {
+      throw new Error("Not a valid status.");
+    }
   }
 
-  if (query.type && ALLOWED_TYPES.includes(query.type)) {
-    match.type = query.type;
+  if (query.type) {
+    if (ALLOWED_TYPES.includes(query.type)) {
+      match.type = query.type;
+    } else {
+      throw new Error("Not a valid type.");
+    }
   }
 
-  if (query.date === "newest") sort.createdAt = -1;
-  if (query.date === "oldest") sort.createdAt = 1;
-
-  const isValidDate = (dateString: string) => {
-    const date = new Date(dateString);
-    return !isNaN(date.getTime());
-  };
+  if (query.date) {
+    if (query.date === "newest") {
+      sort.createdAt = -1;
+    } else if (query.date === "oldest") {
+      sort.createdAt = 1;
+    } else {
+      throw new Error("Not a valid sorting parameter");
+    }
+  }
 
   if (query.startDate && query.endDate) {
     if (
